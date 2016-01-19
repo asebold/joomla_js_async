2356f1d7ed10cd8b22726e0c5190bded0cd604d2
Allison Seboldt <aseboldt@enterprisecruises.com>
2016-01-19 16:36:15 -0600

    added initial files for plugin

---
+++ js_async/js_async.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allisonseboldt
+ * Date: 10/14/15
+ * Time: 3:56 PM
+ */
+
+defined('_JEXEC') or die;
+
+class plgSystemJs_async extends JPlugin
+{
+    public function onBeforeCompileHead() {
+        $app = JFactory::getApplication();
+        /* does not run on administrator panel to prevent users from locking themselves out */
+        if(!$app->isAdmin()) {
+            $files = explode(',', $this->params->get('file_names'));
+            $document = JFactory::getDocument();
+            if ($this->params->get('file_names')) {
+                foreach ($document->_scripts as $url => $values) {
+                    foreach ($files as $keyword) {
+                        if (strpos($url, $keyword) !== false) {
+                            $document->_scripts[$url]['async'] = 'async';
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
